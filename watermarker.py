783eaf1f56822ca5c369bdf93cb0273d86d2d5b4
Chav Sumalinog <chaviotics@gmail.com>
2021-02-05 20:26:39 +0800

    Version 1.0

---
+++ watermarker.py
@@ -0,0 +1,16 @@
+import PyPDF2
+
+# use wtr.pdf
+
+template = PyPDF2.PdfFileReader(open('Merged PDFs.pdf', 'rb'))
+watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
+
+output = PyPDF2.PdfFileWriter()
+
+for i in range(template.getNumPages()):
+    page = template.getPage(i)
+    page.mergePage(watermark.getPage(0))
+    output.addPage(page)
+
+    with open('watermarked output.pdf', 'wb') as file:
+        output.write(file)
